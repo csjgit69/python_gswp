c6904ad099e204316dcd17e25eea96d8d5aa66b2
csjgit69 <csjgit@gmail.com>
2017-09-16 16:50:23 -0700

    week 5 eclispe bs

--- print_test/test_package/test_package.py
+++ print_test/test_package/test_package.py
@@ -5,3 +5,25 @@ Created on Sep 16, 2017
 '''
 
 print("hello world")
+_spam = 3
+#spam = 3
+x = 1 + 2 * 3 - 8 / 4
+print(x)
+x = int(98.6)
+print(x)
+x = int(99.9)
+print(x)
+x = "35"
+y = "2.75"
+#x = float(x)
+#y = float(y)
+z = float(y)*float(x)
+print("Pay: ",z)
+
+z=-1
+if (z>0):
+    print("greater 0")
+elif (z==0):
+    print("equal 0")
+else:
+    print("less 0")
