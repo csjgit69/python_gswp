a4c3a02718a86aaeb5fe403b5d0729e926c03cf4
csjgit69 <csjgit@gmail.com>
2017-09-17 14:26:40 -0700

    python for everyone course 1 done

---
+++ src/chapter4_6.py
@@ -0,0 +1,31 @@
+import platform
+import sys
+import datetime
+from pip.utils.ui import RateLimiter
+from audioop import ratecv
+
+# 4.6 Write a program to prompt the user for hours and rate per hour using raw_input to compute gross pay. 
+# Award time-and-a-half for the hourly rate for all hours worked above 40 hours.
+# Put the logic to do the computation of time-and-a-half in a function called computepay()
+# and use the function to do the computation. The function should return a value.
+# Use 45 hours and a rate of 10.50 per hour to test the program (the pay should be 498.75).
+# You should use raw_input to read a string and float() to convert the string to a number.
+# Do not worry about error checking the user input unless you want to - you can assume the user types numbers properly.
+# Do not name your variable sum or use the sum() function.
+
+def computepay(h,r):
+    pay = h*(r*1.5)
+    return pay
+
+hrs = input("Enter Hours: ")
+rate = input("Enter Rate: ")
+hrs = float(hrs)
+rate = float(rate)
+if (hrs>40):
+    rpay = 40*rate
+    opay = computepay(hrs-40, rate)
+else:
+    rpay = hrs*rate
+    opay = 1    
+pay = rpay + opay
+print ("Pay",pay)
--- src/test.py
+++ src/test.py
@@ -24,3 +24,18 @@ print("Today is: {0:%a %b %d %H:%M:%S %Y}".format(datetime.datetime.now()))
 
 print("My name is {0:8}".format('Fred'))
 
+friends = ['Joseph', 'Glenn', 'Sally']
+for friend in friends :
+     print('Happy New Year:',  friend)
+print('Done!')
+
+zork = 0
+for thing in [9, 41, 12, 3, 74, 15] :
+    zork = zork + thing
+print('After', zork)
+
+smallest_so_far = -1
+for the_num in [9, 41, 12, 3, 74, 15] :
+   if the_num < smallest_so_far :
+      smallest_so_far = the_num
+print(smallest_so_far)
